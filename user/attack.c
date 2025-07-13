6daed40f5167bda80de75d358b22b862e3f58b8c
painjoker <painjoker42@gmail.com>
2025-07-13 20:53:14 +0800

    ✨ feat: figure out why fuzzy search

--- user/attack.c
+++ user/attack.c
@@ -4,7 +4,7 @@
 #include "kernel/riscv.h"
 
 #define PGNUM 32
-#define PROMPTLEN 32
+#define PROMPTLEN 31
 
 int
 main(int argc, char *argv[])
@@ -22,7 +22,25 @@ main(int argc, char *argv[])
 
     // Must use fuzzy search; too accurate will fail
     // because of memory contamination?
-    if(match_count > 20) {
+    if(match_count >= 23) {
+      // debug
+      // the reason why fuzzy search is
+      // 8 byte pointer allocate when free a page.
+      printf("Found at page %d, matches: %d/31\n", i, match_count);
+      printf("Pattern: ");
+      
+      char output[32];
+      for(int k = 0; k < 32; k++) {
+        char ch = end[i * PGSIZE + k];
+        if(ch >= 32 && ch <= 126) {
+          output[k] = ch;
+        } else {
+          output[k] = '.';  // substitution of unprintable char
+        }
+      }
+      output[31] = '\0';
+      printf("%s\n", output);
+
       write(2, end + i * PGSIZE + 32, 8);
       exit(0);
     }
